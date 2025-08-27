bb8e1e9bd76855ef0ee90e478be1a171120ce88a
Subhakanta Lenka <subhakanta.lenka@paritycube.com>
2025-08-27 17:25:17 +0530

    added app.js and index.css

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello Created my first react code") //{} is for giving attributes to a class
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading) //render take on object and convert to tag and also replace is already anything there
+
+// to Add nested elements
+// const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
+//     React.createElement("h1", {id: "heading"}, "Tried nested elements")
+// ))
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(parent)
+
+
+//to Add siblings put it in array
+// const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
+//     [React.createElement("h1", {id: "heading"}, "Tried nested elements"),
+//         React.createElement("h2", {id: "heading2"}, "Tried sibbling")]
+//     )
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(parent)
