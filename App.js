851d0fbe36fe1460d5d05004a160ddb7ee7b5a54
Subhakanta Lenka <subhakanta.lenka@paritycube.com>
2025-09-04 21:32:01 +0530

    Added parcel and learnt about parcel

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1", {id: "heading"}, "Hello Created my first react code") //{} is for giving attributes to a class
 // const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading) //render take on object and convert to tag and also replace is already anything there
+// root.render(heading) //render take on object and convert to tag and also replace is already anything
 
 // to Add nested elements
 // const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
@@ -11,10 +13,10 @@
 
 
 //to Add siblings put it in array
-// const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
-//     [React.createElement("h1", {id: "heading"}, "Tried nested elements"),
-//         React.createElement("h2", {id: "heading2"}, "Tried sibbling")]
-//     )
-// )
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(parent)
+const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
+    [React.createElement("h1", {id: "heading", key: "h1"}, "Tried nested elements"),
+        React.createElement("h2", {id: "heading2", key: "h2"}, "Tried sibbling")]
+    )
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
---
+++ readme.md
@@ -0,0 +1,16 @@
+#parcel
+ Dev build
+ local server
+ hot module replacement
+ File watching algorithm- written in C++
+ caching-faster builds
+ image optimization
+ minification
+ bundling
+ compressing
+ consistent hashing
+ code splitting
+ differential bundling. - support older browsers
+ allows to host in https
+ tree shaking - remove unused code
+ different dev and prod bundles
