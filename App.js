8a038f2e6fe0223660a2106f5717db2408c71525
Subhakanta Lenka <subhakanta.lenka@paritycube.com>
2025-09-07 15:33:08 +0530

    started a food delivert app.

--- App.js
+++ App.js
@@ -1,33 +1,67 @@
 //JSX react
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-// //React Element
-// const jsxHeading = (<h1 id="heading" 
-//     className= "test">
-//         Namaste React with JSX</h1>);
+/**
+ * Header
+ * -logo
+ * -nav items
+ * Body
+ * -search
+ * -restaurant container
+ *   -restaurant card
+ * footer
+ * -copyright
+ * -links
+ * -address
+ */
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
-
-//React component
-const number = 100;
-const Element1 = (
-    <h1> Trying element inside component</h1>
+const Header = () => (
+    <div className = "header">
+        <img className = "logo" src = "https://img.freepik.com/premium-vector/savorbite-kitchen-food-company-logo-design_1059123-403.jpg" />
+        <div className = "nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
 )
-const Title = () => (
-    <div id="container">
-        <h1>Namste react using JSX</h1>
-        <h2>{number}</h2>
-    </div>
-);
 
-const HeadingComponent = () => (
-    <div>
-        {Element1}
-        <Title />
-    <h1> Namaste React functional component</h1>
+
+
+const RestraurantCard = (props) => (
+  <div className = "restraunt-card">
+    <img className = "restraunt-logo" src = {props.image} />
+    <ul> 
+      <li>{props.res_name}</li>
+      <li>{props.cuisine}</li>
+      <li>{props.rating}</li>
+    </ul>
+  </div>
+)
+
+const Body = () =>(
+  <div className = "body">
+    <div className = "search"></div>
+    <div className = "restraunt-container">
+      <RestraurantCard res_name="Meghana foods" 
+      cuisine="Biriyani" image="https://bonmasala.com/wp-content/uploads/2022/10/mutton-biriyani-recipe.jpeg" rating="4.5" />
+      <RestraurantCard res_name="Ambur Foods"
+      cuisine="Chinese street foods"  image = "https://images.unsplash.com/photo-1585032226651-759b368d7246?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hpbmVzZSUyMGZvb2R8ZW58MHx8MHx8fDA%3D"
+      rating="3.5"/>
     </div>
-);
+  </div>
+)
+
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
